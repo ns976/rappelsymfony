8f10aaeca64129d8cd02807f2a19772295f9b18c
Nair Said <nair.said@panoranet.com>
2025-04-09 17:17:49 +0200

    add Product entity and repository; update navbar and home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,6 +1,10 @@
 <?php
 namespace App\Controller;
 
+    use App\Entity\Product;
+    use Cocur\Slugify\Slugify;
+    use Doctrine\ORM\EntityManager;
+    use Doctrine\ORM\EntityManagerInterface;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
@@ -14,6 +18,6 @@ namespace App\Controller;
         public
         function homepage () : Response
         {
-            return $this -> render( 'home.html.twig' );
+            return $this -> render( 'home.html.twig');
         }
     }
