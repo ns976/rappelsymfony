1b626111144e32111422eb79e41720fd4a4e1fc7
Nair Said <nair.said@panoranet.com>
2025-04-09 14:40:31 +0200

    A la découverte de Twig !

--- src/Controller/HelloWordController.php
+++ src/Controller/HelloWordController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use App\Taxes\Detector;
 use Cocur\Slugify\Slugify;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,20 +15,26 @@ class HelloWordController extends AbstractController
 {
 
 
-    public
-    function __construct ()
-    {
-    }
-
-
     /**
      * @Route("/hello/{nom}", name="hello_word")
      */
-    public function index(string $nom="",Calculator $calculator, LoggerInterface $logger,Slugify $slug): Response
+    public function index(string $nom="",Calculator $calculator, LoggerInterface $logger,Slugify $slug,Detector $detector): Response
     {
-        dd($slug->slugify( "hello word "));
-        $tva = $calculator->calcul( 100.00,$logger);
+            dump($slug->slugify( "hello word "));
+            $tva = $calculator->calcul( 100.00,$logger);
+            dump($detector->detect( 100));
+            dump($detector->detect( 110));
+       return $this->render('hello_word/index.html.twig',['nom'=>$nom,'tva'=>$tva]);
+    }
 
-        return $this->render('hello_word/index.html.twig',['nom'=>$nom,'tva'=>$tva]);
+    /**
+     * @Route("/testtwig/{prenom}", name="test_twig")
+     */
+    public function testtwig(string $prenom) : Response{
+
+        $formateur = ['prenom'=>'Jean','nom'=>'Jacques','age'=>12];
+        $formateur_deux = ['prenom'=>'John','nom'=>'Doe','age'=>12];
+
+        return $this->render('hello_word/test.html.twig',["prenom"=>$prenom,"formateur1"=>$formateur,"formateur2"=>$formateur_deux]);
     }
 }
