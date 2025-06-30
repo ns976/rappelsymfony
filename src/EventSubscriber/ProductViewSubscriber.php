c44f4447bf8b46969a34e12e09169d8c4aa64173
Nair Said <nair.said@panoranet.com>
2025-06-30 11:51:14 +0200

    Ajout de la gestion des emails pour les événements de consultation de produit et de succès d'achat, avec des paramètres de configuration pour l'expéditeur.

--- src/EventSubscriber/ProductViewSubscriber.php
+++ src/EventSubscriber/ProductViewSubscriber.php
@@ -4,26 +4,39 @@ namespace App\EventSubscriber;
 
 use App\Event\ProductViewEvent;
 use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
 
 
-class ProductViewSubscriber implements EventSubscriberInterface
+class ProductViewSubscriber extends  AbstractController implements EventSubscriberInterface
 {
     public $logger;
-    public function __construct(LoggerInterface $logger){
+    public $mailer;
+    public function __construct(LoggerInterface $logger , MailerInterface $mailer){
         $this->logger = $logger;
+        $this->mailer = $mailer;
     }
 
 
     public  function sendmailAdministrateur(ProductViewEvent $event){
-            $product = $event->getProduct();
-            $this->logger->info( "Un nouvelle utilisateur a regarde le produit ".$product->getname());
+            $email = new Email();
+              $email->to( 'test@adresse.com')
+                    ->from(new Address( $this->getParameter('no_reply') , 'No Reply'))
+                    ->subject('Un produit a été consulté')
+                    ->text('Un produit a été consulté : ' . $event->getProduct()->getName());
+
+
+             $this->mailer->send($email);
     }
 
     public static function getSubscribedEvents(): array
     {
         return [
-            ProductViewEvent::EVENT_PRODUCT_VIEW => 'sendmailAdministrateur',
+          //  ProductViewEvent::EVENT_PRODUCT_VIEW => 'sendmailAdministrateur',
         ];
     }
 }
--- src/EventSubscriber/purchaseSussesEmailSubscriber.php
+++ src/EventSubscriber/purchaseSussesEmailSubscriber.php
@@ -5,11 +5,12 @@
     use App\Entity\User;
     use App\Event\PurchaseSuccessEvent;
     use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\EventDispatcher\EventSubscriberInterface;
     use Symfony\Component\Mailer\MailerInterface;
     use Symfony\Component\Mime\Address;
 
-    class purchaseSussesEmailSubscriber implements EventSubscriberInterface
+    class purchaseSussesEmailSubscriber extends AbstractController implements EventSubscriberInterface
     {
         private $mail;
 
@@ -29,7 +30,7 @@
             $User     = $Purchase->getUser();
             //--Version email avec template
             $email = (new TemplatedEmail()) -> to(new Address( $User->getEmail(), $User->getFullName()))
-                ->from( 'contact@symshop.com' )
+                ->from( new Address( $this->getParameter( 'no_reply'),$this->getParameter( 'no_reply_name')))
                 ->subject( 'Votre commande  Ecommerce  n° '.$Purchase->getId().'à été expédié' )
                 ->htmlTemplate( "email/purchase_send.html.twig")
                 ->context( ['purchase'=>$Purchase,'user'=>$User]);
